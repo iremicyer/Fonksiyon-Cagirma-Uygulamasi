6d2dd46efcc74e695479407c2927cb4f186ade78
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-13 18:08:04 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fonskiyon_Cagirma
+{
+    class Class1
+    {
+        public static void DegerAl(int [] sayilar)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+
+            }
+            Class1 t = new Class1();
+            t.Topla(sayilar);
+        }
+     private void Topla(int [] sayilar)
+        {
+          
+            int toplam = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                toplam += sayilar[i];
+            }
+
+
+            Console.WriteLine("Dizi içindeki sayılarin toplamı: " + toplam);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fonskiyon_Cagirma
+{
+    // İŞLEM 1: Class1 oluşturuldu.
+    // İŞLEM 2: ekrandan 5 tane int değer alındı.
+    // İŞLEM 3: alınan değerler 2. class' ta ki DegerAl(9 fonksiyonuna gönderildi.
+    // İŞLEM 4: Class1' de private Topla() metodu oluşturuldu.
+    // İŞLEM 5: DegerAl() fonskitonu Topla() fonskiyonuna gönderildi.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sayilar = new int[5];
+
+            // fonksiyon çağırma işlemini dizi tanımladıktan sonra yapmalıyız. Aksi takdirde hata alırız.
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Bir sayi giriniz:");
+
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+          
+            Console.WriteLine("-----------------------------------------");
+            }
+
+            Class1.DegerAl(sayilar); // sayilar dizisi içindeki değerleri Class1' de ki DegerAl() fonksiyonuna gönderiyoruz.
+
+
+            
+
+
+
+        }
+    }
+}
